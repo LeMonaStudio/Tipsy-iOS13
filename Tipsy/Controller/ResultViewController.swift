8467f63c944a9d977e23d7170c3d37c7328fdb2a
Musa Lawrence Ilemona <musa.lawrence93@gmail.com>
2023-03-24 14:40:00 +0100

    Create a ResultViewController class using the Cocoa Touch class and linked the Result view to it from the Main storyboard.

---
+++ Tipsy/Controller/ResultViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Musa Lawrence on 3/24/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
